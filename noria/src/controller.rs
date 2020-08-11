e4c8c4134aeef430657f7f70b8fafd8427a523e5
Eleonora Kiziv <eleonora_kiziv@brown.edu>
2020-08-11 13:04:40 -0400

    Updates the table and view function to work in release mode

--- noria/src/controller.rs
+++ noria/src/controller.rs
@@ -303,23 +303,21 @@ impl<A: Authority + 'static> ControllerHandle<A> {
         // limit_mutator_creation test in src/controller/handle.rs should then be updated as well.
         #[cfg(debug_assertions)]
         // assert_infrequent::at_most(400);
-        let views = self.views.clone();
         let name = name.to_string();
-        self.handle
+
+        let body = self
+            .handle
             .call(ControllerRequest::new("view_builder", &name).unwrap())
             .map_err(|e| format_err!("failed to fetch view builder: {:?}", e))
-            .and_then(move |body: hyper::Chunk| {
-                match serde_json::from_slice::<Option<ViewBuilder>>(&body) {
-                    Ok(Some(vb)) => {
-                        future::Either::A(vb.build(views).map_err(failure::Error::from))
-                    }
-                    Ok(None) => {
-                        future::Either::B(future::err(failure::err_msg("view does not exist")))
-                    }
-                    Err(e) => future::Either::B(future::err(failure::Error::from(e))),
-                }
-                .map_err(move |e| e.context(format!("building view for {}", name)).into())
-            })
+            .wait()
+            .unwrap();
+        let views = self.views.clone();
+        match serde_json::from_slice::<Option<ViewBuilder>>(&body) {
+            Ok(Some(vb)) => future::Either::A(vb.build(views).map_err(failure::Error::from)),
+            Ok(None) => future::Either::B(future::err(failure::err_msg("view does not exist"))),
+            Err(e) => future::Either::B(future::err(failure::Error::from(e))),
+        }
+        // .map_err(move |e| e.context(format!("building table for {}", name)).into())
     }
 
     /// Obtain a `Table` that allows you to perform writes, deletes, and other operations on the
@@ -329,30 +327,30 @@ impl<A: Authority + 'static> ControllerHandle<A> {
     pub fn table(
         &mut self,
         name: &str,
-    ) -> impl Future<Item = Table, Error = failure::Error> + Send {
+    ) -> impl Future<Item = Table, Error = failure::Error> + Send + 'static {
         // This call attempts to detect if this function is being called in a loop. If this
         // is getting false positives, then it is safe to increase the allowed hit count.
         #[cfg(debug_assertions)]
         // assert_infrequent::at_most(400);
-        let domains = self.domains.clone();
         let name = name.to_string();
-        self.handle
+        let body = self
+            .handle
             .call(ControllerRequest::new("table_builder", &name).unwrap())
             .map_err(|e| format_err!("failed to fetch table builder: {:?}", e))
-            .and_then(move |body: hyper::Chunk| {
-                match serde_json::from_slice::<Option<TableBuilder>>(&body) {
-                    Ok(Some(tb)) => future::Either::A(
-                        tb.build(domains)
-                            .into_future()
-                            .map_err(failure::Error::from),
-                    ),
-                    Ok(None) => {
-                        future::Either::B(future::err(failure::err_msg("view table not exist")))
-                    }
-                    Err(e) => future::Either::B(future::err(failure::Error::from(e))),
-                }
-                .map_err(move |e| e.context(format!("building table for {}", name)).into())
-            })
+            .wait()
+            .unwrap();
+        let domains = self.domains.clone();
+
+        match serde_json::from_slice::<Option<TableBuilder>>(&body) {
+            Ok(Some(tb)) => future::Either::A(
+                tb.build(domains)
+                    .into_future()
+                    .map_err(failure::Error::from),
+            ),
+            Ok(None) => future::Either::B(future::err(failure::err_msg("view table not exist"))),
+            Err(e) => future::Either::B(future::err(failure::Error::from(e))),
+        }
+        // .map_err(move |e| e.context(format!("building table for {}", name)).into())
     }
 
     // TODO: we can't use impl Trait here, because it would assume that the returned future is tied
